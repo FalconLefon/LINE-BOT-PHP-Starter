d24f9304b60f02f0801527bfc483fa5f832d0f50
FalconLefon <32950126+FalconLefon@users.noreply.github.com>
2017-10-20 19:15:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,9 +11,9 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'รักเฌอนะครับ<3') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'รักนะครับ<3') {
 			// Get text sent
-			$text = $event['message']['บร้าาาาาาาาา'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
