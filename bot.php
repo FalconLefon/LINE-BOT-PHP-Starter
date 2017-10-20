662ac9d46da6bea1536b69e62a5be482a4778bac
FalconLefon <32950126+FalconLefon@users.noreply.github.com>
2017-10-20 14:00:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,13 +13,13 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event['message']['สวัสดีค่ะ'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
+				'type' => 'สวัสดีครับ',
 				'text' => $text
 			];
 
